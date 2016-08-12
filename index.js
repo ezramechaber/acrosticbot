6fe1aa22c2ed3a40988eb91bdcaba8c4b5bcd754
Ezra Mechaber <ezramechaber@Ezras-MacBook-Pro.local>
2016-08-12 00:40:32 -0400

    Use function to write poem, use another function to handle any starting word

--- index.js
+++ index.js
@@ -1,139 +1,65 @@
+var twitterAPI = require('node-twitter-api');
+var fs = require('fs');
+var word = "alphabet";
+var poem = "";
+
 String.prototype.toProperCase = function () {
     return this.replace(/\w\S*/g, function(txt){return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();});
 };
 
-// function flip(number) {
-// 	return Math.floor(Math.random() * number) === 0;
+function lookupWord(letter,wordList,wordPick) {
+	var randomnumber = Math.floor(Math.random() * 2) === 0;
+	if (randomnumber == 0) {
+	    //true
+		var dict = fs.readFileSync('words.txt', 'utf8');
+		var regEx = new RegExp('(\\b' + letter + ')\\w+', 'g');
+		wordList = dict.match(regEx);
+		wordPick = wordList[Math.floor(Math.random() * wordList.length)].toProperCase();
+		return wordPick;
+	}
+
+	else { 
+	    //false
+	    wordPick = letter.toProperCase();
+	    return wordPick;
+	}
+
+ }
+
+function writePoem (word) {
+	//poem = lookupWord('b') + "\n" + lookupWord('e') + "\n" + lookupWord('n') + "\n" + lookupWord('g') + "\n" + lookupWord('h') + "\n" + lookupWord('a') + "\n" + lookupWord('z') + "\n" + lookupWord('i');
+	//return poem;
+	for (var i = 0, len = word.length; i < len; i++) {
+		poem = poem + lookupWord(word[i]) + "\n";
+	}
+	console.log(poem);
+	return poem;
+}
+
+writePoem (word);
+
+//poem = lookupWord('b') + "\n" + lookupWord('e') + "\n" + lookupWord('n') + "\n" + lookupWord('g') + "\n" + lookupWord('h') + "\n" + lookupWord('a') + "\n" + lookupWord('z') + "\n" + lookupWord('i');
+//console.log(writePoem());
+
+
+// function main() {
+//     var accessToken = process.argv[4];
+//     var tokenSecret = process.argv[5];
+
+//     var twitter = new twitterAPI({
+//         consumerKey: process.argv[2],
+//         consumerSecret: process.argv[3]});
+
+//     twitter.statuses("update",
+//         {"status": writePoem()},
+//         accessToken,
+//         tokenSecret,
+//         function(error, data, response) {
+//             if (error) {
+//                 console.log("something went wrong: " + util.inspect(error));
+//             }
+//         }
+//     );
 // }
 
-// function lookupWord(letter, words, word) {
-// 		var regEx = /(\bletter)\w+/g;
-// 	    var words = data.match(regEx);
-// 	    word = words[Math.floor(Math.random() * words.length)].toProperCase();
-//  }
-
-var fs = require('fs');
-var bword = "B";
-var eword = "E";
-var nword = "N";
-var gword = "G";
-var hword = "H";
-var aword = "A";
-var zword = "Z";
-var iword = "I";
-var poem = "";
-// var randomnumber = flip(2)
-
-
-// read our text file
-fs.readFile('words.txt', 'utf8', function(error, data) {
-	
-	// Letter B
-	randomnumber = Math.floor(Math.random() * 2) === 0;
-	if (randomnumber == 0) {
-	    //true
-		var regExE = /(\bb)\w+/g;
-	    var bletters = data.match(regExE);
-	    bword = bletters[Math.floor(Math.random() * bletters.length)].toProperCase();
-	}
-
-	else { 
-	    //false
-	}
-
-	// Letter E
-	randomnumber = Math.floor(Math.random() * 2) === 0;
-	if (randomnumber == 0) {
-	    //true
-		regExE = /(\be)\w+/g;
-	    var eletters = data.match(regExE);
-	    eword = eletters[Math.floor(Math.random() * eletters.length)].toProperCase();
-	}
-
-	else { 
-	    //false
-	}
-
-	// Letter N
-	randomnumber = Math.floor(Math.random() * 2) === 0;
-	if (randomnumber == 0) {
-	    //true
-		regExE = /(\bn)\w+/g;
-	    var nletters = data.match(regExE);
-	    nword = nletters[Math.floor(Math.random() * nletters.length)].toProperCase();
-	}
-
-	else { 
-	    //false
-	}
-
-	// Letter G
-	randomnumber = Math.floor(Math.random() * 2) === 0;
-	if (randomnumber == 0) {
-	    //true
-		regExE = /(\bg)\w+/g;
-	    var gletters = data.match(regExE);
-	    gword = gletters[Math.floor(Math.random() * gletters.length)].toProperCase();
-	}
-
-	else { 
-	    //false
-	}
-
-	// Letter H
-	randomnumber = Math.floor(Math.random() * 2) === 0;
-	if (randomnumber == 0) {
-	    //true
-		regExE = /(\bh)\w+/g;
-	    var hletters = data.match(regExE);
-	    gword = hletters[Math.floor(Math.random() * hletters.length)].toProperCase();
-	}
-
-	else { 
-	    //false
-	}
-
-	// Letter A
-	randomnumber = Math.floor(Math.random() * 2) === 0;
-	if (randomnumber == 0) {
-	    //true
-		regExE = /(\ba)\w+/g;
-	    var aletters = data.match(regExE);
-	    gword = aletters[Math.floor(Math.random() * aletters.length)].toProperCase();
-	}
-
-	else { 
-	    //false
-	}
-
-	// Letter Z
-	randomnumber = Math.floor(Math.random() * 2) === 0;
-	if (randomnumber == 0) {
-	    //true
-		regExE = /(\bz)\w+/g;
-	    var zletters = data.match(regExE);
-	    gword = zletters[Math.floor(Math.random() * zletters.length)].toProperCase();
-	}
-
-	else { 
-	    //false
-	}
-
-	// Letter I
-	randomnumber = Math.floor(Math.random() * 2) === 0;
-	if (randomnumber == 0) {
-	    //true
-		regExE = /(\bi)\w+/g;
-	    var iletters = data.match(regExE);
-	    gword = iletters[Math.floor(Math.random() * iletters.length)].toProperCase();
-	}
-
-	else { 
-	    //false
-	}
-
-	poem = bword + "\n" + eword + "\n" + nword + "\n" + gword + "\n" + hword + "\n" + aword + "\n" + zword + "\n" + iword;
-	console.log (poem)
-
-}
-);
+// main();
--- sandbox.js
+++ sandbox.js
@@ -1,45 +1,26 @@
-//var Promise = require("bluebird");
 var fs = require('fs');
-var b;
-var e;
-var n;
-var gword;
-var hword;
-var aword;
-var zword;
-var iword;
 var poem;
-
-//Promise.promisifyAll(fs);
 
 String.prototype.toProperCase = function () {
     return this.replace(/\w\S*/g, function(txt){return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();});
 };
 
-function lookupWord(letter,words,word) {
+function lookupWord(letter,wordList,wordPick) {
 	var randomnumber = Math.floor(Math.random() * 2) === 0;
 	if (randomnumber == 0) {
 	    //true
 		var dict = fs.readFileSync('words.txt', 'utf8');
 		var regEx = new RegExp('(\\b' + letter + ')\\w+', 'g');
-		words = dict.match(regEx);
-		word = words[Math.floor(Math.random() * words.length)].toProperCase();
-		//console.log(word);
-		// fs.readFile('words.txt', 'utf8', function lookup(error, data) {
-		// 	    var regEx = new RegExp('(\\b' + letter + ')\\w+', 'g');
-		// 	    //console.log(data);
-		// 	    var words = data.match(regEx);
-		// 	    var word = words[Math.floor(Math.random() * words.length)].toProperCase();
-		// 	    console.log(word);
-		// 	});
-		return word;
+		wordList = dict.match(regEx);
+		wordPick = wordList[Math.floor(Math.random() * wordList.length)].toProperCase();
+		return wordPick;
 	}
 
 
 	else { 
 	    //false
-	    word = letter.toProperCase();
-	    return word;
+	    wordPick = letter.toProperCase();
+	    return wordPick;
 	}
 
  }
