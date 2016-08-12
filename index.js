d63d7db90243efb45946e8fc31eebb1ec0ca773e
Ezra Mechaber <ezramechaber@Ezras-MacBook-Pro.local>
2016-08-12 01:08:04 -0400

    cleanup

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var twitterAPI = require('node-twitter-api');
 var fs = require('fs');
-var word = "alphabet";
+var word = "benghazi";
 var poem = "";
 
 String.prototype.toProperCase = function () {
@@ -27,39 +27,30 @@ function lookupWord(letter,wordList,wordPick) {
  }
 
 function writePoem (word) {
-	//poem = lookupWord('b') + "\n" + lookupWord('e') + "\n" + lookupWord('n') + "\n" + lookupWord('g') + "\n" + lookupWord('h') + "\n" + lookupWord('a') + "\n" + lookupWord('z') + "\n" + lookupWord('i');
-	//return poem;
 	for (var i = 0, len = word.length; i < len; i++) {
 		poem = poem + lookupWord(word[i]) + "\n";
 	}
-	console.log(poem);
 	return poem;
 }
 
-writePoem (word);
+function main() {
+    var accessToken = process.argv[4];
+    var tokenSecret = process.argv[5];
 
-//poem = lookupWord('b') + "\n" + lookupWord('e') + "\n" + lookupWord('n') + "\n" + lookupWord('g') + "\n" + lookupWord('h') + "\n" + lookupWord('a') + "\n" + lookupWord('z') + "\n" + lookupWord('i');
-//console.log(writePoem());
+    var twitter = new twitterAPI({
+        consumerKey: process.argv[2],
+        consumerSecret: process.argv[3]});
 
+    twitter.statuses("update",
+        {"status": writePoem()},
+        accessToken,
+        tokenSecret,
+        function(error, data, response) {
+            if (error) {
+                console.log("something went wrong: " + util.inspect(error));
+            }
+        }
+    );
+}
 
-// function main() {
-//     var accessToken = process.argv[4];
-//     var tokenSecret = process.argv[5];
-
-//     var twitter = new twitterAPI({
-//         consumerKey: process.argv[2],
-//         consumerSecret: process.argv[3]});
-
-//     twitter.statuses("update",
-//         {"status": writePoem()},
-//         accessToken,
-//         tokenSecret,
-//         function(error, data, response) {
-//             if (error) {
-//                 console.log("something went wrong: " + util.inspect(error));
-//             }
-//         }
-//     );
-// }
-
-// main();
+main();
